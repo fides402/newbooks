9d1bb38a41f622718a060065957fbdeda15a086d
fides402 <135014190+fides402@users.noreply.github.com>
2025-04-17 01:38:20 +0200

    Update update_books.py

--- scripts/update_books.py
+++ scripts/update_books.py
@@ -8,7 +8,8 @@ scripts = [
     "scraper_libri_italiani_links.py",
     "scraper_libri_americani_links.py",
     "organizza_dati_links.py",
-    "genera_books_json.py"
+    "genera_books_json.py",
+    "aggiungi_copertine.py"
 ]
 
 for script in scripts:
